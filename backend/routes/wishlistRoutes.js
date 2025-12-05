1947f34932cd8095be07ce66fcf6c2ff07118a83
Siddharth Gothadiya <sgothadiya@hawk.illinoistech.edu>
2025-12-04 20:26:28 -0600

    backend directory

--- backend/routes/wishlistRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getWishlist, toggleWishlist } from '../controllers/wishlistController.js';
-import { protect } from '../middleware/auth.js';
-const router = express.Router();
-
-router.get('/', protect, getWishlist);
-router.post('/', protect, toggleWishlist);
-
-export default router;
