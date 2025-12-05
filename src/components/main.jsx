dd92af503bb15f3c70d56c5c0e5675e6cce54af6
Siddharth Gothadiya <sgothadiya@hawk.illinoistech.edu>
2025-12-04 21:12:18 -0600

    Updated
    Removed promotional text for New Season Arrivals.

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -13,10 +13,7 @@ const Home = () => {
           />
           <div className="card-img-overlay d-flex align-items-center">
             <div className="container">
-              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
-              <p className="card-text fs-5 d-none d-sm-block ">
-                Celebrate the season with holiday favorites and thoughtful gifts for family and friends.
-              </p>
+              
             </div>
           </div>
         </div>
