00e9f8d18d07ae0a119fb6a8994cea61c33679d6
Siddharth Gothadiya <sgothadiya@hawk.illinoistech.edu>
2025-12-04 19:51:01 -0600

    Updated
    Updated the About page content to reflect the company's mission and vision.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,19 +8,10 @@ const AboutPage = () => {
         <h1 className="text-center">About Us</h1>
         <hr />
         <p className="lead text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-          facere doloremque veritatis odit similique sequi. Odit amet fuga nam
-          quam quasi facilis sed doloremque saepe sint perspiciatis explicabo
-          totam vero quas provident ipsam, veritatis nostrum velit quos
-          recusandae est mollitia esse fugit dolore laudantium. Ex vel explicabo
-          earum unde eligendi autem praesentium, doloremque distinctio nesciunt
-          porro tempore quis eaque labore voluptatibus ea necessitatibus
-          exercitationem tempora molestias. Ad consequuntur veniam sequi ullam
-          tempore vel tenetur soluta dolore sunt maxime aliquam corporis est,
-          quo saepe dolorem optio minus sint nemo totam dolorum! Reprehenderit
-          delectus expedita a alias nam recusandae illo debitis repellat libero,
-          quasi explicabo molestiae saepe, dolorem tempore itaque eveniet quam
-          dignissimos blanditiis excepturi harum numquam vel nihil? Ipsum
+          At ShopEase, we believe shopping should be simple, smart, and seamless.
+What began as a front-end engineering project has grown into a next-generation e-commerce experience powered by AI, real-time technology, and thoughtful design.
+
+Our mission is to make online shopping faster, more intelligent, and more enjoyable for every customer.
         </p>
 
         <h2 className="text-center py-4">Our Products</h2>
